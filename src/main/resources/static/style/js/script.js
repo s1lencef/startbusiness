9a8d0e89f37e0f6f2b3f0fe8a6a9bc2e9a89ec05
Immortal-cyborg <tatanov2@gmail.com>
2023-10-30 01:15:25 +0300

    небольшие изменения для форм авторизации.
    небольшие изменения для форм авторизации. Кнопка забыл пароль нужна?

--- src/main/resources/static/style/js/script.js
+++ src/main/resources/static/style/js/script.js
@@ -4,7 +4,34 @@ window.onload = function () {
     document.getElementById('content').classList.toggle('blur');
     document.getElementById('content').classList.toggle('blur');
     document.getElementById('content').classList.toggle('blur');
+
+    // Get the input element by its ID
+    const inputElement = document.getElementById('password');
+
+    // Create a new MouseEvent object
+    const clickEvent = new MouseEvent('click', {
+    bubbles: true,
+    cancelable: true,
+    view: window
+    });
+
+    // Dispatch the click event on the input element
+    inputElement.dispatchEvent(clickEvent);
 }
+
+function show_hide_password(target){
+    var input = document.getElementById('password');
+    if (input.getAttribute('type') == 'password') {
+        target.classList.add('view');
+        input.setAttribute('type', 'text');
+    } else {
+        target.classList.remove('view');
+        input.setAttribute('type', 'password');
+    }
+    return false;
+}
+
+
 
 let burger = document.getElementById('menu-btn');
 burger.addEventListener('click', () => {
