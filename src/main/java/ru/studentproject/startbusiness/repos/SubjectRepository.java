f91e024a1500a314b2ee35ead0e35d30eabf6ebc
s1lencef <favotyakov@stud.etu.ru>
2023-12-18 00:49:23 +0300

    Сохраняю базюлю

--- src/main/java/ru/studentproject/startbusiness/repos/SubjectRepository.java
+++ src/main/java/ru/studentproject/startbusiness/repos/SubjectRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ru.studentproject.startbusiness.models.Subject;
 
 public interface SubjectRepository extends JpaRepository<Subject,Long> {
+    public Subject findByName(String name);
 }
