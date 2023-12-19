953a21042bea3d2d34459bf8a568c04888a5990c
Immortal-cyborg <tatanov2@gmail.com>
2023-12-20 00:26:28 +0300

    update templates

--- src/main/resources/static/style/js/script.js
+++ src/main/resources/static/style/js/script.js
@@ -261,15 +261,3 @@ for (let i = 0; i < divs.length; i++) {
         }
     });
 }
-
-
-for (let i = 1; i <= 5; i++) {
-    let menu_btn2 = document.getElementById('business-' + i);
-    menu_btn2.addEventListener('click', function() {
-        for (let j = 1; j <= 5; j++) {
-            if(document.getElementById('business-info-' + j).classList.contains('business-info-open')) {
-                document.getElementById('business-info-' + j).classList.toggle('business-info-open');
-            }}
-        document.getElementById('business-info-' + i).classList.toggle('business-info-open');
-    });
-}
