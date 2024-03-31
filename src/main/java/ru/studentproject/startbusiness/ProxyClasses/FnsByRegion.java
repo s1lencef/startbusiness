13fd137ec922894e30e1def1a26c37e517544c2f
s1lencef <favotyakov@stud.etu.ru>
2024-03-31 16:05:23 +0300

    Завершено получение списка отделений ФНС

--- src/main/java/ru/studentproject/startbusiness/ProxyClasses/FnsByRegion.java
+++ src/main/java/ru/studentproject/startbusiness/ProxyClasses/FnsByRegion.java
@@ -1,14 +1,18 @@
 package ru.studentproject.startbusiness.ProxyClasses;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import ru.studentproject.startbusiness.dto.FnsInfoDto;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FnsByRegion extends DadataProxyParent{
-    public int getFnsArray(String region) throws IOException, JSONException {
+    public ArrayList<FnsInfoDto> getFnsArray(String region) throws IOException, JSONException {
         final String urlString = "http://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/fns_unit";
         createConnection(urlString);
         setJSONParameters(region);
@@ -16,20 +20,37 @@ public class FnsByRegion extends DadataProxyParent{
         return readFnsFromBuffer();
 
     }
-    private int readFnsFromBuffer(){
+    private ArrayList<FnsInfoDto> readFnsFromBuffer(){
         try (BufferedReader bf = new BufferedReader(new InputStreamReader(
                 connection.getInputStream())))
         {
             String line = bf.readLine();
             JSONObject json = new JSONObject(line);
-            //Тут дописать логику обработки массива (Делаем свой из номера и адреса)
-            int code = Integer.parseInt(json.getJSONArray("suggestions").getJSONObject(0).getJSONObject("data").get("code").toString());
-            System.out.println(code);
-            return code;
+
+            ArrayList<FnsInfoDto> answers = new ArrayList<FnsInfoDto>();
+            JSONArray suggestions = json.getJSONArray("suggestions");
+            int length = suggestions.length();
+            for (int i = 0; i<length;i++){
+                JSONObject fns = suggestions.getJSONObject(i);
+                JSONObject data = fns.getJSONObject("data");
+                FnsInfoDto fnsInfoDto  = new FnsInfoDto(data);
+                answers.add(fnsInfoDto);
+            }
+            System.out.println(answers);
+            return answers;
 
         }catch (IOException exception){
             System.out.println(exception);
-            return -1;
+            throw new RuntimeException();
+        }
+    }
+
+    public static void main(String[] args) {
+        try{
+            FnsByRegion fnsByRegion = new FnsByRegion();
+            fnsByRegion.getFnsArray("Санкт-Петербург");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
--- src/main/java/ru/studentproject/startbusiness/controllers/DadataProxyController.java
+++ src/main/java/ru/studentproject/startbusiness/controllers/DadataProxyController.java
@@ -1,13 +1,18 @@
 package ru.studentproject.startbusiness.controllers;
 
 import jakarta.validation.Valid;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import ru.studentproject.startbusiness.ProxyClasses.FnsByRegion;
 import ru.studentproject.startbusiness.dto.SubjectRequestDto;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 @Controller
 @RequestMapping("/proxy/dadata")
@@ -15,12 +20,13 @@ public class DadataProxyController {
     JSONObject params;
 
     @GetMapping("/get-fns/region")
-    public ResponseEntity<String> getFnsByRegion(@Valid @RequestBody SubjectRequestDto subjectRequestDto, Model model){
+    public ResponseEntity<?> getFnsByRegion(@Valid @RequestBody SubjectRequestDto subjectRequestDto, Model model) throws IOException {
         System.out.println("subjectRequestDto = " + subjectRequestDto.getSubjectName());
         String subjectName = subjectRequestDto.getSubjectName();
+        FnsByRegion fnsByRegion = new FnsByRegion();
 
 
-        return ResponseEntity.ok("Регион получен");
+        return ResponseEntity.ok(fnsByRegion.getFnsArray(subjectName));
 
     };
 }
